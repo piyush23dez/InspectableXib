f8fc7884f1d5a0290d4d5f7520167ecdfca80b7b
Piyush Sharma <piyush_sharma2@apple.com>
2017-04-09 17:05:11 -0700

    comments

--- InspectableXib/XibView/XibView.swift
+++ InspectableXib/XibView/XibView.swift
@@ -1,28 +1,32 @@
-//
-//  XibView.swift
-//  InspectableXib
-//
-//  Created by Piyush Sharma on 4/9/17.
-//  Copyright © 2017 Piyush Sharma. All rights reserved.
-//
+
+/* 
+   This class loads view from its xib name & add as subview with autoresizing mask for autolayout constraints
+   Step 1: In our xib, setup the File’s Owner with the utility class we create.
+   Step 2: In our storyboard, when we want to use our xib, set its class as XibView & set it’s nibName property as your xib filename in the identity inspector.
+   Step 3: Override prepareForInterfaceBuilder method which will instantiate our view and trigger IB rendering.
+*/
+
 
 import UIKit
 
 @IBDesignable
 class XibView: UIView {
     
+    //contentView will be the view created in xib.
     var contentView: UIView?
-    @IBInspectable var nibName: String?
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        xibSetup()
-    }
+    //nibName is the title of xib file. We want this field accessible from the identity inspector in storyboard
+    @IBInspectable var nibName: String?
     
     override func prepareForInterfaceBuilder() {
         super.prepareForInterfaceBuilder()
         xibSetup()
         contentView?.prepareForInterfaceBuilder()
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        xibSetup()
     }
     
     func xibSetup() {
